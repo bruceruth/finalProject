6c034327a7c587e34dd7a7d2581524ba8ccef3f9
ncistudent1 <ruth.stephenson1@gmail.com>
2016-06-12 22:15:57 +0000

    trying to migrate branch

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160612221105) do
+
+  create_table "appointments", force: :cascade do |t|
+    t.string   "name"
+    t.string   "phone"
+    t.integer  "numpeople"
+    t.date     "date"
+    t.string   "timeslot"
+    t.string   "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
