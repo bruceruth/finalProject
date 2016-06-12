6e4eb0ed0eac18666c43c8ca370a0a5e523a41bd
ncistudent1 <ruth.stephenson1@gmail.com>
2016-06-12 22:12:34 +0000

    trying first appointments scaffold with timeslot as string

---
+++ app/views/appointments/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @appointment, :id, :name, :phone, :numpeople, :date, :timeslot, :comment, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :appointments
   get 'basic_pages/home'
 
   get 'basic_pages/contact'
